70a13f30d183f8e22f2c83d2b8486f01a6cc39eb
salgarsanju@gmail.com <salgarsanju@gmail.com>
2025-04-22 09:53:14 +0530

    main class comment added

--- src/com/sanjay/student/Main.java
+++ src/com/sanjay/student/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//main java file
 		StudentService service = new StudentService();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
